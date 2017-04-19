d0c9d2b85cc5c9a7f1be355831e774f37231dea0
Mohit Vaish <mohit.vaish@emc.com>
2017-04-19 11:45:53 +0530

    Added new test

--- test/bootstrap/node.js
+++ test/bootstrap/node.js
@@ -1,9 +1,11 @@
 //Require the dev-dependencies
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+var expect = chai.expect;
 var server = require('../../index');
 var should = chai.should();
 chai.use(chaiHttp);
+
 
 module.exports = chai;
 module.exports = server;
--- test/parking.test.js
+++ test/parking.test.js
@@ -1,27 +1,35 @@
 /*jshint esversion: 6 */
-var Parking = require('../controllers/mock_parking');
+
 var fs = require('fs');
-var testData;
 //setup chai
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+var expect = chai.expect;
 var server = require('../index');
 var should = chai.should();
 chai.use(chaiHttp);
 
 //User model
-//const Parking = require('../models/parking');
+const Parking = require('../models/parking');
 //Authentication Tests
-// describe('Parking Tests', function(){
-// //  this.timeout(15000);
-//   it('Verify if API are working', function(done){
-//     // chai.request(server)
-//     //     .get('/getMeNearByParkings/')
-//     //     //.query({lng:'77.65',lat:'12.91'})
-//     //     .end(function(err, res) {
-//     //         expect(res).to.have.status(123);
-//     //         done();
-//     //         //set.Timeout(done(),12000);                           // <= Call done to signal callback end
-//     //       });
-//   });
-// });
+describe('Parking Tests', function(){
+this.timeout(15000);
+      it('Verify if getMeNearByParkings API is working', function(done){
+        chai.request(server)
+          .get('/getMeNearByParkings')
+          .query({lng: '77.65', lat: '12.91'})
+          .end(function(err, res) {
+              expect(res).to.have.status(200);
+              done();                                     // <= Call done to signal callback end
+            });
+      });
+      it('Verify if getMeParkingSlot API is working', function(done){
+          chai.request(server)
+            .get('/getMeParkingSlot')
+            .query({parkingSlotId: 1})
+            .end(function(err, res) {
+                expect(res).to.have.status(200);
+                done();                                     // <= Call done to signal callback end
+              });
+          });
+});
